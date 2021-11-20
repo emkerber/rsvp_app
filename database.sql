f656eb97d1f896857286322a6478e24615e9ae97
Liz Kerber <liz@primeacademy.io>
2021-11-20 17:47:51 -0600

    Created database and tables

--- database.sql
+++ database.sql
@@ -5,6 +5,32 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (200) UNIQUE NOT NULL,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "role" VARCHAR (10),
+    "date_added" DATE
+);
+
+CREATE TABLE "guest_list" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (200) UNIQUE NOT NULL,
+    "user_id" INT REFERENCES "user" ON DELETE CASCADE,
+    "attending" VARCHAR (10),
+    "notes_attending" VARCHAR (1000),
+    "diet_restrictions" BOOLEAN,
+    "notes_diet_restrictions" VARCHAR (1000),
+    "add_friends" BOOLEAN,
+    "notes_add_friends" VARCHAR (1000),
+    "sleepover" BOOLEAN,
+    "notes_general" VARCHAR (1000),
+    "last_updated" DATE
+);
+
+CREATE TABLE "pending" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (200) UNIQUE NOT NULL,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "date_added" DATE
 );
