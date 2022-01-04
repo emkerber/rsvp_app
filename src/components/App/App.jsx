724f977335b6c41b5247bb161c1d95d67408ca5f
Liz Kerber <liz@primeacademy.io>
2022-01-04 12:10:13 -0600

    Added /authenticate and styling for MUI Paper around div containers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,8 @@ import {
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { Box, Paper } from '@mui/material';
+
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 
@@ -17,8 +19,7 @@ import ThankYou from '../ThankYou/ThankYou';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
+import AuthenticatePage from '../AuthenticatePage/AuthenticatePage';
 import GuestGuestList from '../GuestGuestList/GuestGuestList';
 import Pending from '../Pending/Pending';
 import Denied from '../Denied/Denied';
@@ -41,134 +42,123 @@ function App() {
     <Router>
       <div>
         <Nav />
-        <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+        <Paper className="paper-container" elevation={6}>
+          <Switch>
+            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/thanks will show the ThankYou page. */}
-          <Route
-            // shows ThankYou at all times (logged in or not)
-            exact
-            path="/thanks"
-          >
-            <ThankYou />
-          </Route>
+            {/* For protected routes, the view could show one of several things on the same route.
+              Visiting localhost:3000/user will show the UserPage if the user is logged in.
+              If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
+              Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+            <ProtectedRoute
+              // logged in shows UserPage else shows LoginPage
+              exact
+              path="/user"
+            >
+              <UserPage />
+            </ProtectedRoute>
 
-          {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows InfoPage else shows LoginPage
+              exact
+              path="/info"
+            >
+              <InfoPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows GuestGuestList else shows LoginPage
+              exact
+              path="/list"
+            >
+              <GuestGuestList />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows GuestGuestList else shows LoginPage
-            exact
-            path="/list"
-          >
-            <GuestGuestList />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows AdminGuestList else shows LoginPage
+              exact
+              path="/admin/guests"
+            >
+              <AdminGuestList />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows AdminGuestList else shows LoginPage
-            exact
-            path="/admin/guests"
-          >
-            <AdminGuestList />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows AdminAddRemove else shows LoginPage
+              exact
+              path="/admin/edit"
+            >
+              <AdminAddRemove />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows AdminAddRemove else shows LoginPage
-            exact
-            path="/admin/edit"
-          >
-            <AdminAddRemove />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows AdminPending else shows LoginPage
+              exact
+              path="/admin/approve"
+            >
+              <AdminPending />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows AdminPending else shows LoginPage
-            exact
-            path="/admin/approve"
-          >
-            <AdminPending />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows Pending else shows LoginPage
+              exact
+              path="/pending"
+            >
+              <Pending />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows Pending else shows LoginPage
-            exact
-            path="/pending"
-          >
-            <Pending />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows Denied else shows LoginPage
+              exact
+              path="/nope"
+            >
+              <Denied />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows Denied else shows LoginPage
-            exact
-            path="/nope"
-          >
-            <Denied />
-          </ProtectedRoute>
+            {/* Visiting localhost:3000/thanks will show the ThankYou page. */}
+            <Route
+              // shows ThankYou at all times (logged in or not)
+              exact
+              path="/thanks"
+            >
+              <ThankYou />
+            </Route>
 
-          <Route
-            exact
-            path="/login"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the login page
-              <LoginPage />
-            }
-          </Route>
+            <Route
+              exact
+              path="/authenticate"
+            >
+              {user.id ?
+                // If the user is already logged in, 
+                // redirect to the /user page
+                <Redirect to="/user" />
+                :
+                // Otherwise, show the login/registration page
+                <AuthenticatePage />
+              }
+            </Route>
 
-          <Route
-            exact
-            path="/registration"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the registration page
-              <RegisterPage />
-            }
-          </Route>
+            <Route
+              exact
+              path="/home"
+            >
+              {user.id ?
+                // If the user is already logged in, 
+                // redirect them to the /user page
+                <Redirect to="/user" />
+                :
+                // Otherwise, show the Landing page
+                <LandingPage />
+              }
+            </Route>
 
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
-            }
-          </Route>
+            {/* If none of the other routes matched, we will show a 404. */}
+            <Route>
+              <h1>Four oh four.</h1>
+            </Route>
 
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>Four oh four.</h1>
-          </Route>
-        </Switch>
+          </Switch>
+        </Paper>
         <Footer />
       </div>
     </Router>
