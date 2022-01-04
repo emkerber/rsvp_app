d69a19d29cbe5db53d80266bd70d623426b28fef
Liz Kerber <liz@primeacademy.io>
2022-01-04 16:32:34 -0600

    Combined guestList reducer and pendingList reducer into one

---
+++ src/redux/reducers/lists.reducer.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux';
+
+const guestList = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_GUEST_LIST':
+      return action.payload;
+    case 'EMPTY_GUEST_LIST':
+      return [];
+    default:
+      return state;
+  }
+};
+
+const pendingList = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PENDING_LIST':
+      return action.payload;
+    case 'EMPTY_PENDING_LIST':
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  guestList,
+  pendingList,
+});
