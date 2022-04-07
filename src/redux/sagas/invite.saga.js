54971e25b9866f5b032a7b60163c8a1c9783902e
Liz Kerber <liz@primeacademy.io>
2022-04-06 19:42:19 -0500

    Now consistently dispatching fullName as an object instead of as a string, and added code comments to invite saga

--- src/redux/sagas/invite.saga.js
+++ src/redux/sagas/invite.saga.js
@@ -3,44 +3,56 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* checkInvite(action) {
   try {   
-    const currentName = action.payload;
+    // the concatenated first and last names that were entered on the Landing Page
+    const currentName = action.payload.fullName;
 
     const config = {
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true,
     };
 
+    // get guest list in db
     const guestList = yield axios.get('/api/guestList', config);
     
+    // loop through guest list
+    // if a name on the guest list matches the currentName entered on the DOM
+    // then set invite-status reducer to 'guest'
     for (let person of guestList.data) {
       if (person.name === currentName) {
         yield put({ type: 'SET_INVITE_STATUS', payload: 'guest' });
-        yield put({ type: 'SET_RESPONSES', payload: person });
+        yield put({ type: 'SET_RESPONSES', payload: person }); // TODO why??
         console.log('Guest!');
         return true;
       }
     }
     
+    // get pending list in db
     const pendingList = yield axios.get('/api/pendingList', config);
 
+    // loop through pending list
+    // if a name on the pending list matches the currentName entered on the DOM
+    // then check if their pending status is resolved
+    // if resolved, then set invite-status reducer to 'nope'
+    // if not resolved, then set invite-status reducer to 'pending'
     for (let person of pendingList.data) {
       if (person.name === currentName) {
-        yield put({ type: 'SET_RESPONSES', payload: person });
+        yield put({ type: 'SET_RESPONSES', payload: person }); // TODO why??
         if (person.resolved) {
           yield put({ type: 'SET_INVITE_STATUS', payload: 'nope' });
-          console.log('NOPE!');
+          console.log('no thx');
           return true;
         } else {
           yield put({ type: 'SET_INVITE_STATUS', payload: 'pending' });
-          console.log('Pending!');
+          console.log('Limbo!');
           return true;
         }
       }
     }
 
-    // if none of the other conditions are hit, then
+    // if the name entered on the Landing Page does not match a name on the guest list nor the pending list,
+    // then set invite-status-reducer to 'none'
     yield put({ type: 'SET_INVITE_STATUS', payload: 'none' });
-    console.log('They\'re not on a list!');
+    console.log('Not on a list!');
 
   } catch (error) {
     console.log('Check invite failed:', error);
