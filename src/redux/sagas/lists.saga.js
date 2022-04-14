fcbff723af3ca8cd22d4e0a50821ec0eba45dc15
Liz Kerber <liz@primeacademy.io>
2022-04-14 18:26:16 -0500

    Comment out code that is unused but may be used later

--- src/redux/sagas/lists.saga.js
+++ src/redux/sagas/lists.saga.js
@@ -3,43 +3,43 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 // might not use these?
 
-function* fetchGuestList() {
-  try {
-    yield put({ type: 'EMPTY_GUEST_LIST' });
+// function* fetchGuestList() {
+//   try {
+//     yield put({ type: 'EMPTY_GUEST_LIST' });
     
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
-    };
+//     const config = {
+//       headers: { 'Content-Type': 'application/json' },
+//       withCredentials: true,
+//     };
     
-    const response = yield axios.get('/api/guestList', config);
+//     const response = yield axios.get('/api/guestList', config);
   
-    yield put({ type: 'SET_GUEST_LIST', payload: response.data });
-  } catch (error) {
-    console.log('Get Guest List request failed:', error);
-  }
-}
+//     yield put({ type: 'SET_GUEST_LIST', payload: response.data });
+//   } catch (error) {
+//     console.log('Get Guest List request failed:', error);
+//   }
+// }
 
-function* fetchPendingList() {
-  try {
-    yield put({ type: 'EMPTY_PENDING_LIST' });
+// function* fetchPendingList() {
+//   try {
+//     yield put({ type: 'EMPTY_PENDING_LIST' });
 
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
-    };
+//     const config = {
+//       headers: { 'Content-Type': 'application/json' },
+//       withCredentials: true,
+//     };
 
-    const response = yield axios.get('/api/pendingList', config);
+//     const response = yield axios.get('/api/pendingList', config);
 
-    yield put({ type: 'SET_PENDING_LIST', payload: response.data });
-  } catch (error) {
-    console.log('Get Pending List request failed:', error);
-  }
-}
+//     yield put({ type: 'SET_PENDING_LIST', payload: response.data });
+//   } catch (error) {
+//     console.log('Get Pending List request failed:', error);
+//   }
+// }
 
 function* listsSaga() {
-  yield takeLatest('FETCH_GUEST_LIST', fetchGuestList);
-  yield takeLatest('FETCH_PENDING_LIST', fetchPendingList);
+  // yield takeLatest('FETCH_GUEST_LIST', fetchGuestList);
+  // yield takeLatest('FETCH_PENDING_LIST', fetchPendingList);
 }
 
 export default listsSaga;
