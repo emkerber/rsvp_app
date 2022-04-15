a4dfbab803dc6d0e49dc012c41a39408f801a1de
Liz Kerber <liz@primeacademy.io>
2022-04-15 11:51:21 -0500

    If a user is logged in, show the nav menu in the nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,60 +7,47 @@ import './Nav.css';
 
 function Nav() {
   const user = useSelector((store) => store.user);
-  const [openMenu, setOpenMenu] = React.useState(false);
-  const prevOpenMenu = React.useRef(openMenu);
-  const anchorRef = React.useRef(null);
+  // const [openMenu, setOpenMenu] = React.useState(false);
+  // const prevOpenMenu = React.useRef(openMenu);
+  // const anchorRef = React.useRef(null);
 
-  const handleToggle = () => {
-    setOpenMenu((prevOpenMenu) => !prevOpenMenu);
-  };
+  // const handleToggle = () => {
+  //   setOpenMenu((prevOpenMenu) => !prevOpenMenu);
+  // };
 
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
+  // const handleClose = (event) => {
+  //   if (anchorRef.current && anchorRef.current.contains(event.target)) {
+  //     return;
+  //   }
+  //   setOpenMenu(false);
+  // };
 
-    setOpenMenu(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpenMenu(false);
-    } else if (event.key === 'Escape') {
-      setOpenMenu(false);
-    }
-  }
+  // function handleListKeyDown(event) {
+  //   if (event.key === 'Tab') {
+  //     event.preventDefault();
+  //     setOpenMenu(false);
+  //   } else if (event.key === 'Escape') {
+  //     setOpenMenu(false);
+  //   }
+  // }
 
   // return focus to the button when we transitioned from !openMenu -> openMenu
-  React.useEffect(() => {
-    if (prevOpenMenu.current === true && openMenu === false) {
-      anchorRef.current.focus();
-    }
-    prevOpenMenu.current = openMenu;
-  }, [openMenu]);
+  // React.useEffect(() => {
+  //   if (prevOpenMenu.current === true && openMenu === false) {
+  //     anchorRef.current.focus();
+  //   }
+  //   prevOpenMenu.current = openMenu;
+  // }, [openMenu]);
 
   return (
     <div className="nav">
+      
+      {/* super cool neon sign-effect logo */}
       <h2 className="nav-title">BRINGOL</h2>
-      <div>
-        {/* If no user is logged in, show Back link to landing page */}
-        {/* {user.id === null &&
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        } */}
+      
+      {/* If a user is logged in, show the nav menu */}
+      {user.id && <NavMenu />}
 
-        {/* If a user is logged in, show the nav menu */}
-        {/* {user.id && (
-          <NavMenu/>
-        )} */}
-
-        {/* <Link className="navLink" to="/about">
-          About
-        </Link> */}
-      </div>
     </div>
   );
 }
