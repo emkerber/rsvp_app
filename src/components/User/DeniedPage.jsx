35ad2e9e04a42ae6b851d45bc6d86fb385312908
Liz Kerber <liz@primeacademy.io>
2022-04-17 13:55:10 -0500

    Denial page displays message that's unique to the user

--- src/components/User/DeniedPage.jsx
+++ src/components/User/DeniedPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Denied() {
+  const denialMessage = useSelector(store => store.invite.responses.denial_message);
+  
   return (
     <div className="container">
-      <h2>You have been denied admittance to this Bringol.</h2>
+      <h1>Admittance denied.</h1>
+      {denialMessage && 
+        <p>{denialMessage}</p>
+      }
     </div>
   );
 }
