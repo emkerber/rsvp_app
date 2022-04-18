844ee90a2d588622f00f1305e15158ccc64a834a
Liz Kerber <liz@primeacademy.io>
2022-04-17 22:03:08 -0500

    Beginning of Rsvp Form

--- src/components/User/Guest/RsvpPage.jsx
+++ src/components/User/Guest/RsvpPage.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import LogOutButton from '../../Authenticate/LogOutButton';
-import {useSelector} from 'react-redux';
+import RsvpForm from './RsvpForm';
 
 function RsvpPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      
+      <h1>RSVP</h1>
+
+      <RsvpForm />
+    
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default RsvpPage;
