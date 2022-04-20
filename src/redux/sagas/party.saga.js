37bbcd0fcafbbb53e43c569e2b8f9c2414106cbc
Liz Kerber <liz@primeacademy.io>
2022-04-19 19:21:10 -0500

    Fix comment

--- src/redux/sagas/party.saga.js
+++ src/redux/sagas/party.saga.js
@@ -1,7 +1,7 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-// worker Saga: will be fired on "REGISTER" actions
+// worker Saga: will be fired on Landing Page
 function* fetchParty() {
   try {
     const config = {
