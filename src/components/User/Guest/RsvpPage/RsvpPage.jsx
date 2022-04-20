db5e19404c1fe965d6b57e0ae078329fd7bb18c9
Liz Kerber <liz@primeacademy.io>
2022-04-19 19:24:05 -0500

    Remove unused code

--- src/components/User/Guest/RsvpPage/RsvpPage.jsx
+++ src/components/User/Guest/RsvpPage/RsvpPage.jsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 
 import RsvpAttending from './RsvpAttending';
 import WelcomeMessage from './WelcomeMessage';
 
-import { FormControl, FormGroup, InputLabel, Input, Select, MenuItem } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import './RsvpPage.css';
 
 function RsvpPage() {
-  const responses = useSelector(store => store.invite.responses);
 
   return (
     <div className="container">
