8ce4f764550dbad10e9120c448b6ddaf37d4e1b0
Liz Kerber <liz@primeacademy.io>
2022-04-24 15:15:15 -0500

    Fix action type after refactor

--- src/redux/sagas/login.saga.js
+++ src/redux/sagas/login.saga.js
@@ -57,7 +57,7 @@ function* logoutUser(action) {
     // also empty the reducers I made
     yield put({ type: 'UNSET_NAME' });
     yield put({ type: 'UNSET_INVITE_STATUS' });
-    yield put({ type: 'UNSET_RESPONSES' });
+    yield put({ type: 'UNSET_GUEST_RESPONSES' });
     yield put({ type: 'ALL_RESPONSES_UNSURE' });
 
   } catch (error) {
