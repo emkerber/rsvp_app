ac6dca9122c6ad4958f0df03752af21c364271de
Liz Kerber <liz@primeacademy.io>
2022-04-27 23:04:53 -0500

    Duties save and update server routes

--- server/routes/duties.router.js
+++ server/routes/duties.router.js
@@ -27,6 +27,66 @@ router.get('/fetch-by-id/:id', rejectUnauthenticated, (req, res) => {
     });
 });
 
+// save new responses 
+// (guest has not previously indicated duty preferences)
+router.post('/save', rejectUnauthenticated, (req, res) => {
+  const queryText = `
+    INSERT INTO "duties"
+      (guest_id, setup, cleanup, hydration, photography, none)
+    VALUES 
+      ($1, $2, $3, $4, $5, $6);
+  `;
 
+  const rb = req.body;
+  const queryParams = [
+    rb.guestId, 
+    rb.setupDuty, 
+    rb.cleanupDuty, 
+    rb.waterDuty, 
+    rb.photoDuty, 
+    rb.noDuty
+  ];
+
+  pool
+    .query(queryText, queryParams)
+    .then(() => res.sendStatus(200))
+    .catch((error) => {
+      console.log('Error saving new duties:', error);
+      res.sendStatus(500);
+    });
+});
+
+// update duty responses 
+// (guest has previously indicated duty preferences)
+router.put('/update', rejectUnauthenticated, (req, res) => {
+  const queryText = `
+    UPDATE "duties" 
+    SET
+      setup = $1,
+      cleanup = $2,
+      hydration = $3,
+      photography = $4,
+      none = $5
+    WHERE guest_id = $6;
+  `;
+
+  const rb = req.body;
+  const queryParams = [
+    rb.setupDuty, 
+    rb.cleanupDuty, 
+    rb.waterDuty, 
+    rb.photoDuty, 
+    rb.noDuty,
+    rb.guestId
+  ];
+
+  pool
+    .query(queryText, queryParams)
+    .then(() => res.sendStatus(200))
+    .catch((error) => {
+      console.log('Error updating guest duties:', error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
