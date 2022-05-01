857e4096f01902cad0acfa170dfaa5a74b609a52
Liz Kerber <liz@primeacademy.io>
2022-05-01 17:09:45 -0500

    No need to fetch guest's duties when they visit the Deets page - it happens when their name is collected when they first visit the app

--- src/components/User/Guest/DeetsPage.jsx
+++ src/components/User/Guest/DeetsPage.jsx
@@ -1,15 +1,6 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
 function DeetsPage() {
-  const dispatch = useDispatch();
-  const dutiesIndicated = useSelector(store => store.guest.responses.duties_indicated);
-  const guestId = useSelector(store => store.guest.responses.id);
-
-  useEffect(() => {
-    dutiesIndicated &&
-      dispatch({ type: 'FETCH_GUEST_DUTIES', payload: guestId });
-  }, [dutiesIndicated]);
 
   return (
     <div className="container">
