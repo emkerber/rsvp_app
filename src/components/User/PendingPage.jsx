9efbad431d17f3b0b29a5af462e218b9df1a0675
Liz Kerber <liz@primeacademy.io>
2022-05-08 17:38:54 -0500

    Collect email on Pending page

--- src/components/User/PendingPage.jsx
+++ src/components/User/PendingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PendingEmail from './PendingEmail';
 
 function Pending() {
   return (
     <div className="container">
+      
       <h2>Your status is pending.</h2>
+      
       <p>Thank you for your patience!</p>
+
+      <PendingEmail />
+
     </div>
   );
 }
