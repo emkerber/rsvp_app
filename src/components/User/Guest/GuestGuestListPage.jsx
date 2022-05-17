8edafe39a3f6332c12621bd2ce90da0836fb3af9
Liz Kerber <liz@primeacademy.io>
2022-05-17 14:36:50 -0500

    Display list of Guests for Guest users who have provided all responses

--- src/components/User/Guest/GuestGuestListPage.jsx
+++ src/components/User/Guest/GuestGuestListPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function GuestGuestList() {
+  const list = useSelector(store => store.guest.guestsList);
+  
   return (
     <div className="container">
-      <h1>The Guest List</h1>
+      
+      <h2>The Guest List</h2>
+
+      <ul>
+        {list.map((guest, i) => (
+          <li key={i}>{guest.guest}</li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
