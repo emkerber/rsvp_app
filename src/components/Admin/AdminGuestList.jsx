9c2316b47b14865664f489f26f778d21df616ce2
Liz Kerber <liz@primeacademy.io>
2022-05-31 18:07:55 -0500

    Table for admin guest list for counts of attending, maybe, not attending, and no response

--- src/components/Admin/AdminGuestList.jsx
+++ src/components/Admin/AdminGuestList.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import AdminGuestTotalsTable from './AdminGuestTotalsTable';
 
 function AdminGuestList() {
   return (
     <div className="container">
+      
+      {/* table with counts for each category */}
+      <AdminGuestTotalsTable />
+
+      <p>Click on a guest's name to see their details.</p>
+
       <h2>Attending</h2>
 
       <h2>Might attend</h2>
@@ -10,6 +17,7 @@ function AdminGuestList() {
       <h2>Not attending</h2>
 
       <h2>No response</h2>
+
     </div>
   );
 }
