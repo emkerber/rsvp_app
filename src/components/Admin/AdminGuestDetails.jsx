d04207b8dfca9a001570c7fc9ac4529aa21276f3
Liz Kerber <liz@primeacademy.io>
2022-05-31 20:11:48 -0500

    Guest details populating the guest details page, and its back button works

--- src/components/Admin/AdminGuestDetails.jsx
+++ src/components/Admin/AdminGuestDetails.jsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { Button } from '@mui/material';
 
 function AdminGuestDetails() {
   const history = useHistory();
-  const guestDetails = useSelector(store => store.guest.guestDetails);
+  const dispatch = useDispatch();
+  const guest = useSelector(store => store.guest.guestDetails);
 
   const handleBackClick = () => {
-    console.log('back!');
+    dispatch({ type: 'UNSET_GUEST_DETAILS' });
+    history.push('/admin/guests');
   }
-  
+
+  const determineStatus = () => {
+    switch (guest?.attending_code) {
+      case 'YAY':
+        return <h3>Attending</h3>;
+      case 'TBD':
+        return <h3>Maybe attending</h3>;
+      case 'NAY':
+        return <h3>Not attending</h3>;
+      default:
+        return <h3>No response</h3>;
+    }
+  }
+
   return (
     <div className="container">
-      <h2>Deets!</h2>
       
+      {/* back button to admin guest list */}
       <Button
         variant="contained"
         onClick={handleBackClick}
       >
         Back
       </Button>
+
+      {/* first and last names */}
+      <h2>{guest?.first_name} {guest?.last_name}</h2>
+
+      {/* Attending / Maybe attending / Not attending / No response */}
+      {determineStatus()}
+
+      {/* questions/comments/concerns */}
+      {guest?.questions_comments && <p>{guest?.first_name} says: {guest?.questions_comments}</p>}
+
+      {/* if they're a Maybe and they provided more info */}
+      {guest?.attending_deets && <p>Attendance deets: {guest?.attending_deets}</p>}
+
+      {/* info regarding people they plan to bring */}
+      {guest?.additional_guests && <p>Additional guest info: {guest?.additional_guests}</p>}
+
+      {/* dietary restriction info */}
+      {guest?.dietary_restrictions && <p>Dietary restrictions: {guest?.dietary_restrictions}</p>}
+
+      {/* parking needs */}
+      {guest?.parking && <p>Parking: {guest?.parking}</p>}
+
+      {/* TODO duties */}
+
+      {/* their unique welcome message */}
+      {guest?.welcome_message && <p>Their welcome message: {guest?.welcome_message}</p>}
+
+      {/* guest's email */}
+      {guest?.email && <p>Email: {guest?.email}</p>}
+
 
     </div>
   );
