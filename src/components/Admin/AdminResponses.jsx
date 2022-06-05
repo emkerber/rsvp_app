98188e687d8607a6f2efa921a7ab7e64ef1e83e6
Liz Kerber <liz@primeacademy.io>
2022-06-05 02:22:21 -0500

    Render lists of guests who responded to each question

--- src/components/Admin/AdminResponses.jsx
+++ src/components/Admin/AdminResponses.jsx
@@ -1,23 +1,21 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import AdminResponseSubList from './AdminResponseSubList';
 
 function AdminResponses() {
-  const dispatch = useDispatch();
+  // data fetched on admin login
+  const dietaryRestrictions = useSelector(store => store.guest.dietaryRestrictions);
+  const parkingDuring = useSelector(store => store.guest.parkingDuring);
+  const parkingOvernight = useSelector(store => store.guest.parkingOvernight);
+  const additionalGuests = useSelector(store => store.guest.additionalGuests);
+  const volunteersSetup = useSelector(store => store.duties.volunteersSetup);
+  const volunteersCleanup = useSelector(store => store.duties.volunteersCleanup);
+  const volunteersHydration = useSelector(store => store.duties.volunteersHydration);
+  const volunteersPhotography = useSelector(store => store.duties.volunteersPhotography);
+  const volunteersNone = useSelector(store => store.duties.volunteersNone);
+  const questionsComments = useSelector(store => store.guest.questionsComments);
 
-  // const dietaryRestrictions = useSelector(store => store.guest.dietaryRestrictions);
-  // const parkingDuring = useSelector(store => store.guest.parkingDuring);
-  // const parkingOvernight = useSelector(store => store.guest.parkingOvernight);
-  // const additionalGuests = useSelector(store => store.guest.additionalGuests);
-  // const dutySetup = useSelector(store => store.guest.dutySetup);
-  // const dutyCleanup = useSelector(store => store.guest.dutyCleanup);
-  // const dutyHydration = useSelector(store => store.guest.dutyHydration);
-  // const dutyPhotography = useSelector(store => store.guest.dutyPhotography);
-  // const dutyNone = useSelector(store => store.guest.dutyNone);
-  // const questionsComments = useSelector(store => store.guest.questionsComments);
-
-  useEffect(() => {
-    // dispatch fetches for each response
-  }, []);
 
   return (
     <div className="container">
@@ -25,21 +23,31 @@ function AdminResponses() {
       <p>Click on a guest's name to see their details.</p>
 
       <h2>Dietary Restrictions</h2>
+      <AdminResponseSubList list={dietaryRestrictions} />
 
       <h2>Parking</h2>
       <h3>During the party only</h3>
+      <AdminResponseSubList list={parkingDuring} />
       <h3>Overnight</h3>
+      <AdminResponseSubList list={parkingOvernight} />
 
       <h2>Additional guests</h2>
+      <AdminResponseSubList list={additionalGuests} />
 
-      <h2>Duty signups</h2>
+      <h2>Volunteers</h2>
       <h3>Setup Squad</h3>
+      <AdminResponseSubList list={volunteersSetup} />
       <h3>Cleanup Crew</h3>
+      <AdminResponseSubList list={volunteersCleanup} />
       <h3>Hydration Helper</h3>
+      <AdminResponseSubList list={volunteersHydration} />
       <h3>Photography Friend</h3>
+      <AdminResponseSubList list={volunteersPhotography} />
       <h3>No thanks to all of those</h3>
+      <AdminResponseSubList list={volunteersNone} />
 
       <h2>Questions / comments / concerns / compliments</h2>
+      <AdminResponseSubList list={questionsComments} />
 
     </div>
   );
