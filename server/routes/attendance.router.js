559897f13145351df1086bad926675263fad325a
Liz Kerber <emkerber@outlook.com>
2023-09-22 15:32:45 -0500

    List attendance server routes I need to create

--- server/routes/attendance.router.js
+++ server/routes/attendance.router.js
@@ -7,3 +7,17 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectNonBouncer } = require('../modules/authentication-middleware');
 
+// TODO - GET guests not checked in
+// guests RIGHT (?) INNER JOIN attendance ON guests.id = guests_id
+
+// TODO - GET guests checked in
+
+// TODO - POST into attendance when guest is checked in
+
+// TODO - DELETE from attendance to un-check-in guest
+
+// TODO - PUT attendance notes
+
+// TODO - GET by search term
+
+// TODO - POST into attendance for new person not in guests table
