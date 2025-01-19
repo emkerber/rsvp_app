8fac19aa1c559336f0f82cc7e61e88b11993ca77
Liz Kerber <emkerber@outlook.com>
2025-01-18 18:42:57 -0600

    Update SELECTS from guests table to specify all fields except phone, rather than selecting all (*) fields

--- server/routes/guests.router.js
+++ server/routes/guests.router.js
@@ -37,7 +37,11 @@ router.get('/search/:party/:firstName/:lastName', (req, res) => {
 // fetch guest's responses by guest.id
 router.get('/fetch-by-id/:id', rejectUnauthenticated, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE id = $1;
   `;
   pool
@@ -53,7 +57,11 @@ router.get('/fetch-by-id/:id', rejectUnauthenticated, (req, res) => {
 // fetch guest's responses by guest.user_id
 router.get('/fetch-by-user-id/:id', rejectUnauthenticated, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE user_id = $1;
   `;
   pool
@@ -134,17 +142,17 @@ router.put('/update-responses/YAY', rejectUnauthenticated, (req, res) => {
   // `;
 
   const queryText = `
-  UPDATE "guests"
-  SET attending = true,
-    attending_code = 'YAY',
-    attending_deets = 'NA',
-    dietary_restrictions = $1,
-    additional_guests = $2,
-    parking = $3,
-    duties_indicated = $4,
-    questions_comments = $5
-  WHERE id = $6;
-`;
+    UPDATE "guests"
+    SET attending = true,
+      attending_code = 'YAY',
+      attending_deets = 'NA',
+      dietary_restrictions = $1,
+      additional_guests = $2,
+      parking = $3,
+      duties_indicated = $4,
+      questions_comments = $5
+    WHERE id = $6;
+  `;
 
   let queryParams = [];
   const rb = req.body;
@@ -216,7 +224,11 @@ router.put('/update-responses/NAY', rejectUnauthenticated, (req, res) => {
 // fetch guests who are attending
 router.get('/admin/attending', rejectNonAdmin, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE attending
     ORDER BY id;
   `;
@@ -234,7 +246,11 @@ router.get('/admin/attending', rejectNonAdmin, (req, res) => {
 // fetch guests who indicated they are maybe attending
 router.get('/admin/maybe', rejectNonAdmin, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE attending_code = 'TBD'
     ORDER BY id;
   `;
@@ -252,7 +268,11 @@ router.get('/admin/maybe', rejectNonAdmin, (req, res) => {
 // fetch guests who have indicated they will not attend
 router.get('/admin/not-attending', rejectNonAdmin, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE attending_code = 'NAY'
     ORDER BY id;
   `;
@@ -270,7 +290,11 @@ router.get('/admin/not-attending', rejectNonAdmin, (req, res) => {
 // fetch guests who have not yet RSVPd
 router.get('/admin/no-response', rejectNonAdmin, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE attending_code IS NULL
     ORDER BY first_name;
   `;
@@ -288,7 +312,11 @@ router.get('/admin/no-response', rejectNonAdmin, (req, res) => {
 // fetch guests who have not yet been invited
 router.get('/admin/not-yet-invited', rejectNonAdmin, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE "invite_sent" = False;
   `;
 
@@ -323,7 +351,11 @@ router.put('/admin/invite-sent/:id', rejectNonAdmin, (req, res) => {
 // fetch details for a guest
 router.get('/admin/details/:id', rejectNonAdmin, (req, res) => {
   const queryText = `
-    SELECT * FROM "guests"
+    SELECT "id", "party_id", "user_id", "first_name", "last_name",
+    "attending", "attending_code", "attending_deets", "dietary_restrictions",
+    "additional_guests", "parking", "duties_indicated", "questions_comments",
+    "welcome_message", "invite_sent"
+    FROM "guests"
     WHERE id = $1;
   `;
 
@@ -491,11 +523,11 @@ router.post('/admin/pending-to-guest', rejectNonAdmin, (req, res) => {
   // `;
 
   const queryText = `
-  INSERT INTO guests
-    (party_id, user_id, first_name, last_name, welcome_message)
-  VALUES
-    ($1, $2, $3, $4, $5);
-`;
+    INSERT INTO guests
+      (party_id, user_id, first_name, last_name, welcome_message)
+    VALUES
+      ($1, $2, $3, $4, $5);
+  `;
 
   const rb = req.body; // pendings fields plus message
   // let queryParams = [rb.party_id, rb.user_id, rb.first_name, rb.last_name, rb.message, rb.phone];
