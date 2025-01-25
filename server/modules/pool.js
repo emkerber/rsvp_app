dee4d94ab0a0f129ebb9ee8fa97ea09ab47e3318
Liz Kerber <lizkerber@Lizs-Laptop.local>
2025-01-25 00:08:15 -0600

    App is connecting to remote db when run locally

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,30 +3,29 @@ const { URL } = require('url');
 
 let config = {};
 
-// AWS gives a url, not a connection object
+// Amazon RDS gives a url, not a connection object
 if (process.env.DATABASE_URL) {
   const params = new URL(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
 
   config = {
-    user: auth[0],
-    password: auth[1],
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: { rejectUnauthorized: false },
+    database: 'rsvp_app',
+    port: 5432,
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 } else {
   config = {
     host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: 'rsvp_app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    port: 5432, 
+    database: 'rsvp_app',
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
-// }
+}
 
 // this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
