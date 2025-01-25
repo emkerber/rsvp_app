ba3e762602b521277fc792e0acd9668cd8578765
Liz Kerber <lizkerber@Lizs-Laptop.local>
2025-01-24 21:46:32 -0600

    Comment-in code used to connect to remote db, and fix depreciated url.parse()

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,25 +1,24 @@
 const pg = require('pg');
-const url = require('url');
+const { URL } = require('url');
 
 let config = {};
 
-// // commented out to ensure use of local db
-// // Heroku and ElephantSQL give a url, not a connection object
-// if (process.env.DATABASE_URL) {
-//   const params = url.parse(process.env.DATABASE_URL);
-//   const auth = params.auth.split(':');
+// AWS gives a url, not a connection object
+if (process.env.DATABASE_URL) {
+  const params = new URL(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
 
-//   config = {
-//     user: auth[0],
-//     password: auth[1],
-//     host: params.hostname,
-//     port: params.port,
-//     database: params.pathname.split('/')[1],
-//     ssl: { rejectUnauthorized: false },
-//     max: 10, // max number of clients in the pool
-//     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-//   };
-// } else {
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: { rejectUnauthorized: false },
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+} else {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
